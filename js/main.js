681df6bad0251564dca2eca5f6eae3f48dad008f
web-verses <gwen15272@gmail.com>
2026-01-24 03:25:20 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+// Load comics from comics.json
+fetch('comics.json')
+  .then(res => res.json())
+  .then(comics => {
+    const list = document.getElementById('comic-list');
+    comics.forEach(comic => {
+      const card = document.createElement('div');
+      card.className = 'comic-card';
+      card.innerHTML = `
+        <img src="${comic.cover}" alt="${comic.title}">
+        <h3>${comic.title}</h3>
+      `;
+      card.onclick = () => {
+        window.location.href = `reader.html?comic=${comic.id}`;
+      };
+      list.appendChild(card);
+    });
+  })
+  .catch(err => console.error('Error loading comics:', err));
 
