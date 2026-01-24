1c3abf9f89755d11a9a61bfab3d4cea1ad3d3edc
web-verses <gwen15272@gmail.com>
2026-01-24 03:25:52 -0500

    Update reader.js

--- js/reader.js
+++ js/reader.js
@@ -1,1 +1,20 @@
+const urlParams = new URLSearchParams(window.location.search);
+const comicId = urlParams.get('comic');
+
+fetch('comics.json')
+  .then(res => res.json())
+  .then(comics => {
+    const comic = comics.find(c => c.id === comicId);
+    const reader = document.getElementById('reader');
+    if (!comic) {
+      reader.innerHTML = 'Comic not found.';
+      return;
+    }
+    comic.pages.forEach(src => {
+      const img = document.createElement('img');
+      img.src = src;
+      reader.appendChild(img);
+    });
+  })
+  .catch(err => console.error('Error loading comic:', err));
 
